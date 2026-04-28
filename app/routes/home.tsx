24837f1467a6b8e23f707a1a1f0404f55d187a0c
aryantiwari-1640 <aryantiwari.1640@gmail.com>
2026-04-29 00:09:21 +0530

    Creating Navbar

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,8 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
 import Navbar from "../../components/Navbar";
+import { ArrowRight, Layers } from "lucide-react";
+import Button from "../../components/ui/Button";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,6 +14,59 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
   return (
   <div className="home"><Navbar/>
-  <h1 className="text-3xl text-indigo-700 font-extrabold">Home</h1>
+  <section className="hero">
+    <div className="announce">
+      <div className="pulse">
+      </div>
+      <p>Introducing Home Designing</p>
+    </div>
+
+    <h1>Build Beautiful Spaces and Experiences</h1>
+    <p className="subtitle">Design and Visualize your dream home with our intuitive tools and expert guidance.</p>
+
+    <div className="actions">
+      <a href="#upload" className="cta">Get Started<ArrowRight className="icon"/></a>
+      <Button variant="outline" className="demo" size="lg">Watch Demo</Button>
+    </div>
+
+    <div id="upload" className="upload-shell">
+       <div className="grid-overlay"/>
+         <div className="upload-card">
+           <div className="upload-head">
+             <div className="upload-icon">
+                <Layers className="icon"/>
+              </div>
+                <h3>Upload your floor plan</h3>
+                <p>Supports JPG,PNG,formats up to 10MB</p>
+            </div>
+
+            <p>Upload Images</p>
+         </div>
+    </div> 
+    
+     
+  </section>
+
+  <section className="projects">
+    <div className="section-inner">
+      <div className="section-head">
+        <div className="copy">
+          <h2>Projects</h2>
+          <p> Your latest work and shared community projects , all in one place</p>
+        </div>
+      </div>
+      <div className="projects-grid">
+        <div className="project-card group">
+          <div className="preview">
+             <img src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png" alt="Project"></img>
+          <div className="badge">
+            <span>Community</span>
+          </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section>
  </div>);
 }
